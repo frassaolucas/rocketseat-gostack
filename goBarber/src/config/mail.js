d534165e61ea6c867f7f0c99350a167e5d6ab3d6
frassaolucas <frassao.lucas@gmail.com>
2020-04-03 11:08:03 -0300

    - Added nodemailer to the project; - Created a method to send and email to notify the provider when a user cancels the appointment; - Configured the mail (config/mail.js); - Configured the class to create a mail (lib/mail.js);

---
+++ goBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "0c565837560725",
+    pass: "770ea5cc70a15d"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
