a7246bb9eaa5e1d2dab956dda6bc61a8144642dc
frassaolucas <frassao.lucas@gmail.com>
2020-04-26 11:53:14 -0300

    - Installed @babel/plugin-proposal-class-properties so it's possible to use short syntax instead of using the constructor for classes; - Created TechList component; - Created TechItem component and connecting it to TechList functions;

--- module-04/src/App.js
+++ module-04/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import profile from "./assets/IMG_1769_Original.jpeg";
+import TechList from "./components/TechList";
 
 function App() {
-  return <img width="200" src={profile} />;
+  return <TechList />;
 }
 
 export default App;
