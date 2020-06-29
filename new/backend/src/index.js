5a152390e9e511d560a96bcb6c0197a0974a784f
frassaolucas <frassao.lucas@gmail.com>
2020-06-29 07:46:05 -0300

    - practicing with parameters;

--- new/backend/src/index.js
+++ new/backend/src/index.js
@@ -2,15 +2,31 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json(['Projecto 1', 'Projeto 2']);
 });
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json(['Projecto 1', 'Projeto 2', 'Projeto 3']);
 });
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json(['Projecto 4', 'Projeto 2', 'Projeto 3']);
 });
 
