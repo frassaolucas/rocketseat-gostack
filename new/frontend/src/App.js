08d20e1d22bf8e08e7e631713330b1e078b3fd91
frassaolucas <frassao.lucas@gmail.com>
2020-06-29 21:40:33 -0300

    - connected backend with frontend to list projects;

--- new/frontend/src/App.js
+++ new/frontend/src/App.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
 
 function App() {
-  const [projects, setProjects] = useState([
-    'Desenvolvimento de app',
-    'Front-end web',
-  ]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
 
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]);
@@ -19,11 +22,9 @@ function App() {
     <>
       <Header title="Projects" />
 
-      <img width={300} src={backgroundImage} />
-
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
