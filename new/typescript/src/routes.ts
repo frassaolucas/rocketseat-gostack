7fcac905f467fc22e030745f79f28bb66f373fc4
frassaolucas <frassao.lucas@gmail.com>
2020-07-02 07:09:46 -0300

    - practicing creating own variable types using typescript;

--- new/typescript/src/routes.ts
+++ new/typescript/src/routes.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import createUser from './services/createUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: 'diego@rocketseat.com.br',
+    password: '123456',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native',
+      { title: 'Javascript', experience: 1000 },
+    ],
+  });
+
   return response.json({ message: 'Hello Worrld' });
 }
