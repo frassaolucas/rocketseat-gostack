e0dd535eec48bf65a4c7a0743f003f98f4de52bd
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-06 09:17:55 -0300

    - changed files to search and save appointments using the database and not just the local variable

--- new/primeiro-projeto-node/src/database/migrations/1594032894822-CreateAppointments.ts
+++ new/primeiro-projeto-node/src/database/migrations/1594032894822-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1594032894822
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
