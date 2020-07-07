f0925ef7f7d1701c88e418659336027d96d57e4d
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-06 21:15:34 -0300

    fixed id types, changed provider to provider_id and fixed model to reflect relation between provider and user

--- new/primeiro-projeto-node/src/database/migrations/1594077603070-CreateUsers.ts
+++ new/primeiro-projeto-node/src/database/migrations/1594077603070-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1594077603070 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
