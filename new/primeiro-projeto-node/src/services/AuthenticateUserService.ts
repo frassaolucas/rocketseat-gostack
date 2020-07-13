8d3b58b08305c7f4b6fe44449ecda005e7890f99
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-13 09:04:32 -0300

    added token to user authentication

--- new/primeiro-projeto-node/src/services/AuthenticateUserService.ts
+++ new/primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'a8b3340e7b8711e8e6b12fc671cb6f70', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
