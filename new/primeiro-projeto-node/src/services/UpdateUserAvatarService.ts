ed41ce686480db522b1c89be13bfba056b3be613
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-14 08:03:53 -0300

    created error class and updated files using this class

--- new/primeiro-projeto-node/src/services/UpdateUserAvatarService.ts
+++ new/primeiro-projeto-node/src/services/UpdateUserAvatarService.ts
@@ -3,6 +3,9 @@ import { getRepository } from 'typeorm';
 import fs from 'fs';
 
 import uploadConfig from '../config/upload';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -17,7 +20,7 @@ class UpdateUserAvatarService {
     const user = await userRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated user can change avatar.');
+      throw new AppError('Only authenticated user can change avatar.', 401);
     }
 
     if (user.avatar) {
