692f2340fb6b2f36ee1b7b6aee02699437a404da
Lucas Frassao <frassao.lucas@gmail.com>
2020-07-16 08:49:09 -0300

    installed styled-components and configured global styles

--- new/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ new/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return <h1>Explore repositórios no Github</h1>;
 };
 
 export default Dashboard;
